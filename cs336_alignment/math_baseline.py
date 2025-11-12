fcb99ed90fedb713abb2a322070f483d6d79dbf4
wren <celticsalan@hotmail.com>
2025-11-11 20:06:15 -0800

    math baseline problem

---
+++ cs336_alignment/math_baseline.py
@@ -0,0 +1,10 @@
+
+
+"""
+1. load the MATH validation examples
+2. format them as string prompts to the language model using the r1_zero prompt
+3. generate outputs for each example
+4. calculate evaluation metrics
+5. serialize the examples, model generations, and corresponding evaluation scores to disk for
+analysis in subsequent problems
+"""
